4bcb9c786a27017f7e7174f6943c24830aa6136b
BLNTTRP <joacocosentino2003@outlook.com.ar>
2026-04-21 10:18:47 -0300

    Database migrated from SQLite to PostgreSQL

--- fastapi-productos/app/core/database.py
+++ fastapi-productos/app/core/database.py
@@ -1,14 +1,20 @@
 import os
 from dotenv import load_dotenv
-from sqlmodel import create_engine, Session, SQLModel
+from sqlmodel import create_engine, Session
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./productos.db")
+DB_USER = os.getenv("POSTGRES_USER", "admin")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "root")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+DB_NAME = os.getenv("POSTGRES_DB", "productos_db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+FINAL_DATABASE_URL = os.getenv("DATABASE_URL", DATABASE_URL)
+
+engine = create_engine(FINAL_DATABASE_URL)
 
 # Dependencia para que FastAPI abra y cierre la conexión en cada petición
 def get_db():
