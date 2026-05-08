5c39788b67400361f25ea66af345e386544caaf9
Joaquin Cosentino <joacocosentino2003@outlook.com.ar>
2026-05-07 23:16:45 -0300

    Refactor: Routing moved to 'src/router/AppRouter.tsx'

--- front-productos/src/App.tsx
+++ front-productos/src/App.tsx
@@ -1,29 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import CategoriasPage from './pages/CategoriasPage';
-import ProductsPage from './pages/ProductsPage';
-import IngredientesPage from './pages/IngredientesPage';
+import AppRouter from './router/AppRouter';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div className="min-h-screen bg-gray-50">
-                <Navbar />
-
-                <main>
-                    <Routes>
-                        {/* Redirección: si entran a localhost:5173/ los manda a /categorias */}
-                        <Route path="/" element={<Navigate to="/categorias" replace />} />
-
-                        {/* Rutas de las páginas con soporte opcional para ID */}
-                        <Route path="/categorias/:id?" element={<CategoriasPage />} />
-                        <Route path="/productos/:id?" element={<ProductsPage />} />
-                        <Route path="/ingredientes/:id?" element={<IngredientesPage />} />
-                    </Routes>
-                </main>
-            </div>
-        </BrowserRouter>
-    );
+    return <AppRouter />;
 }
 
 export default App;
